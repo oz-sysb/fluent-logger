d58d26731d779df265546eea05d32451fdfbd3ea
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2020-05-29 13:24:37 +0900

    [MOD] phpunit で使う bootstrap, mock, 設定ファイルを配置

---
+++ mock/MockLogger.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Mock Fluent Logger
+ */
+class MockLogger implements Fluent\Logger\LoggerInterface
+{
+    private
+        $key,
+        $log;
+
+
+    private static $forceException = false;
+
+    /**
+     * send a message to specified fluentd.
+     *
+     * @param string $tag
+     * @param array  $data
+     * @return bool
+     *
+     * @api
+     */
+    public function post($tag, array $data)
+    {
+        if (self::$forceException) {
+            throw new \Exception('forced error');
+        }
+
+        $this->key = $tag;
+        $this->log = $data;
+    }
+
+    /**
+     * send a message to specified fluentd.
+     *
+     * @param Entity $entity
+     * @return bool
+     */
+    public function post2(Fluent\Logger\Entity $entity)
+    {
+        return true;
+    }
+
+    public function getLastKey()
+    {
+        return $this->key;
+    }
+
+    public function getLastLog()
+    {
+        return $this->log;
+    }
+
+    public static function forceException($condition = false)
+    {
+        $default = self::$forceException;
+        self::$forceException = $condition;
+
+        return $default;
+    }
+}
